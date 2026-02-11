ea88366fabe82d2d703181282e66662550277ea9
JelencsityMiklous <jelencsitymiklos@turr.hu>
2026-02-11 08:44:44 +0100

    Szükséges komponensek és mappaszerkeszet létrehozása

---
+++ server.js
@@ -0,0 +1,22 @@
+const app = require('./Backend/config/app');
+const {sequelize} = require('./Backend/models/index');
+
+const PORT = process.env.PORT;
+
+(async () =>{
+try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+    
+    sequelize.sync({alter:true});
+
+    app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+
+});
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+    process.exit(1);
+}
+
+})();
