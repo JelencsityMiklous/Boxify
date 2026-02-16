8d94152cc1e6e245655c833138178c5a408b2036
acsnorbert <acsnorbert@turr.hu>
2026-02-16 11:13:22 +0100

    Hibák fixálása frontendbe

---
+++ Frontend/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig } from '@angular/core';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/lara';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    providePrimeNG({
+      theme: {
+        preset: Lara
+      }
+    })
+  ]
+};
